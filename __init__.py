0ef2fdf2c32e3238a0d1697f160b72bf57f0f9fd
Sos1ska <72695378+Sos1ska@users.noreply.github.com>
2023-02-19 18:19:36 +0300

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,3 @@
-from .modules.api.break_ip import BreakIPAddress
-from .modules.api.break_mac import BreakMACAddress
-from .modules.api.break_number import BreakNumberPhone
+from .modules.api.break_ip import BreakIPAddress, BreakIPAddress_modules
+from .modules.api.break_mac import BreakMACAddress, BreakMACAddress_modules
+from .modules.api.break_number import BreakNumberPhone, BreakNumberPhone_modules
